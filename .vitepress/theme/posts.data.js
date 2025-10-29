19c570e2a1a9f99a88c123968eac850861ae847c
tatapinmatteoward <athcdejd81rul5@hotmail.com>
2025-10-29 11:15:07 +0000

    Add .vitepress/theme/posts.data.js

---
+++ .vitepress/theme/posts.data.js
@@ -0,0 +1,45 @@
+export default {
+  load() {
+    return [
+    {
+        "url": "/blog/10-natural-remedies-colds-flu-everyday-herbs",
+        "title": "10 Natural Remedies for Colds and Flu: Harnessing Everyday Herbs",
+        "excerpt": "Discover effective, natural ways to combat colds and flu symptoms with 10 everyday herbs like ginger, garlic, and peppermint. Boost your immunity naturally and feel better fast!",
+        "date": "2023-10-15",
+        "tags": [
+            "herbal remedies",
+            "natural health",
+            "colds",
+            "flu",
+            "immunity"
+        ]
+    },
+    {
+        "url": "/blog/holistic-sleep-hacks-natures-secrets-for-better-rest",
+        "title": "Holistic Sleep Hacks: Nature's Secrets for Better Rest",
+        "excerpt": "Discover natural, holistic approaches to improve your sleep quality using nature's wisdom, from herbal remedies to mindful practices for rejuvenating rest.",
+        "date": "2023-10-15",
+        "tags": [
+            "sleep",
+            "holistic-health",
+            "natural-remedies",
+            "wellness",
+            "rest"
+        ]
+    },
+    {
+        "url": "/blog/digestive-health-boost-herbal-remedies-upset-stomach",
+        "title": "Digestive Health Boost: Herbal Remedies for Upset Stomach",
+        "excerpt": "Discover natural herbal remedies to soothe and restore digestive health. From ginger to peppermint, learn safe, effective ways to alleviate upset stomach symptoms with nature's remedies.",
+        "date": "2025-10-29",
+        "tags": [
+            "herbal-remedies",
+            "digestive-health",
+            "natural-medicines",
+            "upset-stomach",
+            "wellness"
+        ]
+    }
+]
+  }
+}
